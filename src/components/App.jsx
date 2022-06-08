56a54c7529f2273353e133101e7a16c07c3c283a
valiantlynx <86688436+valiantlynx@users.noreply.github.com>
2022-06-08 23:44:32 +0200

    use of more props and fatarrowfunction

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import Heading from './Header';
-import Footer from './Footer';
+import React from "react";
+import Heading from "./Header";
+import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../Note";
 
 function App() {
-    return (
-        <div> 
-            <Heading /> 
-            <Footer /> 
-            <Note /> 
-        </div>
-        );
+  return (
+    <div>
+      <Heading />
+      <Footer />
+      {notes.map((note) => (
+        <Note key={note.key} title={note.title} content={note.content} />
+      ))}
+    </div>
+  );
 }
 
 export default App;
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 
-function Note(){
-    return (
-        <div className="note">
-            <h1>Note title</h1>
-            <p>Note content</p>
-
-        </div>
-        );
+function Note(props) {
+  return (
+    <div className="note">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+    </div>
+  );
 }
 
 export default Note;
